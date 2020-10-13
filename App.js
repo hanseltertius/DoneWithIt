5dac311c290b79086004092b1cad7d634f1dfc75
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-13 16:10:04 +0700

    Implement Alert

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Button } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Button, Alert } from 'react-native';
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button color="orange" title="Click Me" onPress={() => console.log("Button Pressed")} />
+      <Button color="orange" title="Click Me" onPress={() => Alert.alert("My title", "My message", [
+        { text: "Yes", onPress: () => console.log("Yes") },
+        { text: "No", onPress: () => console.log("No") }
+      ])} />
     </SafeAreaView>
   );
 }
