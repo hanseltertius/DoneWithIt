9c650c1cb2a4e410dbb679a16bb24d3f2800b65e
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-13 15:56:23 +0700

    Implement Button

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Button } from 'react-native';
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-        <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}></View>
-      </TouchableNativeFeedback>
+      <Button color="orange" title="Click Me" onPress={() => console.log("Button Pressed")} />
     </SafeAreaView>
   );
 }
