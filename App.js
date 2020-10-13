4dd2fc47839f47abad3cee47c391ff53933a4d14
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-13 15:11:44 +0700

    modify Text component

--- App.js
+++ App.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text Clicked");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text
+        numberOfLines={1}
+        onPress={handlePress}>
+        Hello React Native - A really really long text.
+        Now I wanna make this even longer and see what happens!
+      </Text>
     </SafeAreaView>
   );
 }
