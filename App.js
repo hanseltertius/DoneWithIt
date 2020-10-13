f3c4ea63a689d45f49624eda8f9e240af3b14fa1
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-13 16:42:37 +0700

    Apply Platform Specific Code

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Button, Alert } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Button, Alert, Platform, StatusBar } from 'react-native';
 
 export default function App() {
 
   const containerStyle = { backgroundColor: 'orange' };
 
   return (
-    <View style={[styles.container, containerStyle]}>
+    <SafeAreaView style={styles.container}>
       <Button title="Click Me" onPress={() => Alert.alert("My title", "My message", [
         { text: "Yes", onPress: () => console.log("Yes") },
         { text: "No", onPress: () => console.log("No") }
       ])} />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -20,7 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
