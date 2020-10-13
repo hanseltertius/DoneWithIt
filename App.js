284f2dffc22e940988c1c405c7daeea9927268d0
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-13 15:45:04 +0700

    Implement TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Text, View } from 'react-native';
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
-      <Image
-        fadeDuration={1000}
-        blurRadius={10}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300"
-        }} />
+      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          fadeDuration={1000}
+          blurRadius={10}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }} />
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
