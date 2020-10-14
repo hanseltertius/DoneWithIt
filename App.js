9eb840e422a090a065775553d1aa1fc5fd810ef3
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-14 15:51:30 +0700

    Implement Encapsulating Styles

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+
+import AppText from './app/components/AppText';
 
 export default function App() {
   return (
@@ -10,19 +10,7 @@ export default function App() {
       justifyContent: 'center',
       alignItems: 'center'
     }}>
-      <Text style={{
-        // fontFamily: 'notoserif',
-        fontSize: 30,
-        fontStyle: 'italic',
-        fontWeight: 'bold',
-        color: 'tomato',
-        textTransform: 'capitalize',
-        textDecorationLine: 'none',
-        textAlign: 'center',
-        lineHeight: 40
-      }}>
-        I love React Native! This is my first React Native app! Here's some more text!
-      </Text>
+      <AppText>I love React Native!</AppText>
     </View>
   );
 }
