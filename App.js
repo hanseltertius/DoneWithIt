a3bea5b1ff8ce029f386585b2d13382141e4c80b
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-14 12:05:11 +0700

    Implement Borders

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: 'royalblue',
+          borderRadius: 50,
+        }} />
+    </View>
   );
 }
