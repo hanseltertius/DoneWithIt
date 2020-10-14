f630f2df42feaa8e86719bfb64cad6e20589d440
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-14 13:56:14 +0700

    Apply Padding and Margin

--- App.js
+++ App.js
@@ -15,8 +15,27 @@ export default function App() {
           backgroundColor: 'dodgerblue',
           width: 100,
           height: 100,
-          elevation: 20
+          paddingLeft: 30,
+          padding: 20,
+          paddingHorizontal: 10,
+
+        }}>
+        <View
+          style={{
+            backgroundColor: 'gold',
+            width: 50,
+            height: 50
+          }} />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          width: 100,
+          height: 100,
+          margin: 20,
         }} />
+
     </View>
   );
 }
