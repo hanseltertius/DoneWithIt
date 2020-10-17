5a25e2a02b561d44ccbc69647cc60671f11c9faa
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-17 15:49:48 +0700

    Implement TextInput

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppText from './app/components/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import ListingDetailScreen from './app/screens/ListingDetailScreen';
@@ -9,9 +9,23 @@ import ListItemScreen from './app/screens/ListItemScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import MyAccountScreen from './app/screens/MyAccountScreen';
+import { Text, TextInput } from 'react-native';
+import Screen from './app/components/Screen';
 
 export default function App() {
+
+  const [firstName, setFirstName] = useState('');
+
   return (
-    <ListingsScreen />
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        onChangeText={text => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: '#ccc',
+          borderBottomWidth: 1
+        }} />
+    </Screen>
   );
 }
