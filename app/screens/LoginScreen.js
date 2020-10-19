73b8cd0fa0c0c8deeae4c38d53252597de3037e0
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-19 12:00:26 +0700

    Implement The Touched State

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { Formik } from 'formik';
 import { Image, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 
-import Screen from '../components/Screen';
 import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
 import AppText from '../components/AppText';
+import AppTextInput from '../components/AppTextInput';
 import ErrorMessage from '../components/ErrorMessage';
+import Screen from '../components/Screen';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -27,18 +27,19 @@ function LoginScreen(props) {
                 initialValues={{ email: '', password: '' }}
                 onSubmit={values => console.log(values)}
                 validationSchema={validationSchema}>
-                {({ handleChange, handleSubmit, errors }) => (
+                {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
                     <>
                         <AppTextInput
                             autoCapitalize="none"
                             autoCorrect={false}
                             icon="email"
                             keyboardType="email-address"
+                            onBlur={() => setFieldTouched("email")}
                             onChangeText={handleChange("email")}
                             placeholder="Email"
                             textContentType="emailAddress"
                         />
-                        <ErrorMessage error={errors.email} />
+                        <ErrorMessage error={errors.email} visible={touched.email} />
 
                         <AppTextInput
                             autoCapitalize="none"
@@ -49,7 +50,7 @@ function LoginScreen(props) {
                             secureTextEntry
                             textContentType="password"
                         />
-                        <ErrorMessage error={errors.password} />
+                        <ErrorMessage error={errors.password} visible={touched.password} />
 
                         <AppButton
                             title="Login"
