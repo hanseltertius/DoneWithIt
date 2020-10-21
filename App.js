129ddd196114f2b4d9e561019d5d8df64b8cd28d
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-21 11:10:51 +0700

    Implement Permissions API from Expo

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 import Screen from './app/components/Screen';
 
 export default function App() {
 
   const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
+
+    // const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
   }
 
