a97a568fed1b853984918ed56aa0f86afe378efb
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-21 11:46:24 +0700

    Implement on Accessing the ImagePicker library

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
 import Screen from './app/components/Screen';
+import { Button, Image } from 'react-native';
 
 export default function App() {
 
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
 
-    const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
+    // const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
 
-    // const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
   }
 
@@ -18,7 +21,20 @@ export default function App() {
     requestPermission();
   }, []);
 
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled)
+        setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an Image");
+    }
+  }
+
   return (
-    <Screen></Screen>
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
   )
 }
