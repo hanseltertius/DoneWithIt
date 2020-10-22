750fa051d0d305fba3ff9a98952f04d8e34fa7af
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-22 16:21:52 +0700

    Implement on Customizing Header

--- App.js
+++ App.js
@@ -30,8 +30,18 @@ const TweetDetails = ({ route }) => (
 const Stack = createStackNavigator();
 
 const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}>
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      options={{
+        headerStyle: { backgroundColor: "tomato" },
+        headerTintColor: "white",
+      }} />
     <Stack.Screen
       name="TweetDetails"
       component={TweetDetails}
