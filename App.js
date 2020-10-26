c22bb30eb625477667e0cdfd8f4b64ff2f8a44fd
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-26 22:54:46 +0700

    Implement NetInfo to detect Network Connectivity Status and Changes

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Button } from 'react-native';
+import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 
+
 export default function App() {
 
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+
+  const netInfo = useNetInfo();
+
+  return <Button title="Test" disabled={!netInfo.isInternetReachable} onPress={() => console.log("We connected to internet")} />;
 
 }
+
+// return (
+//   <NavigationContainer theme={navigationTheme}>
+//     <AppNavigator />
+//   </NavigationContainer>
+// );
