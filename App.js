65cbecac3b4a6bdee3c411708228493e3195d797
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-27 10:32:27 +0700

    Implement AsyncStorage

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'react-native';
 import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
@@ -9,10 +10,21 @@ import navigationTheme from './app/navigation/navigationTheme';
 
 export default function App() {
 
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem('person', JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem('person');
+      const person = JSON.parse(value);
 
-  const netInfo = useNetInfo();
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  return <Button title="Test" disabled={!netInfo.isInternetReachable} onPress={() => console.log("We connected to internet")} />;
+  demo();
+
+  return null;
 
 }
 
