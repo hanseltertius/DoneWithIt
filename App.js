748a2f6bd7775b18a5474807ea4454ae702a20d5
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-27 16:48:14 +0700

    Implement solution on exercise

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
-import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 import { NavigationContainer } from '@react-navigation/native';
-import AsyncStorage from '@react-native-community/async-storage';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import OfflineNotice from './app/components/OfflineNotice';
 import Screen from './app/components/Screen';
-import OfflineNoticeBar from './app/components/OfflineNoticeBar';
 
 
 export default function App() {
-
-  const netInfo = useNetInfo();
-
   return (
-    <Screen>
-      { !netInfo.isInternetReachable && <OfflineNoticeBar />}
+    <>
+      <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
         <AppNavigator />
       </NavigationContainer>
-    </Screen>
+    </>
 
   );
 
