9b0995c1ce8554e13b7591002652cb3b92dc7d6a
hanseltortiusdev <hansel.tertius@tortius.id>
2020-10-31 10:20:41 +0700

    Implement on getting a push notification token

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 import AccountNavigator from './AccountNavigator';
 import ListingEditScreen from '../screens/ListingEditScreen';
@@ -10,37 +12,57 @@ import routes from './routes';
 
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-    <Tab.Navigator>
-        <Tab.Screen
-            name={routes.FEED}
-            component={FeedNavigator}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialCommunityIcons
-                        name="home"
-                        color={color}
-                        size={size}
-                    />
-                )
-            }} />
-        <Tab.Screen
-            name={routes.LISTING_EDIT}
-            component={ListingEditScreen}
-            options={({ navigation }) => ({
-                tabBarButton: () => (
-                    <NewListingButton
-                        onPress={() => navigation.navigate(routes.LISTING_EDIT)}
-                    />
-                )
-            })} />
-        <Tab.Screen
-            name={routes.ACCOUNT}
-            component={AccountNavigator}
-            options={{
-                tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home" color={color} size={size} />
-            }} />
-    </Tab.Navigator>
-);
+const AppNavigator = () => {
+
+    useEffect(() => {
+        registerForPushNotifications();
+    }, []);
+
+    const registerForPushNotifications = async () => {
+
+        try {
+            const permissions = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            if (!permissions.granted) return;
+
+            const token = Notifications.getExpoPushTokenAsync();
+            console.log(token);
+        } catch (error) {
+            console.log('Error getting a push token', error);
+        }
+    }
+
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name={routes.FEED}
+                component={FeedNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="home"
+                            color={color}
+                            size={size}
+                        />
+                    )
+                }} />
+            <Tab.Screen
+                name={routes.LISTING_EDIT}
+                component={ListingEditScreen}
+                options={({ navigation }) => ({
+                    tabBarButton: () => (
+                        <NewListingButton
+                            onPress={() => navigation.navigate(routes.LISTING_EDIT)}
+                        />
+                    )
+                })} />
+            <Tab.Screen
+                name={routes.ACCOUNT}
+                component={AccountNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home" color={color} size={size} />
+                }} />
+        </Tab.Navigator>
+    );
+}
 
 export default AppNavigator;
