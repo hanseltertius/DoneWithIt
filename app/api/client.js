8cb4682da8dcff80fddacb296150540ada32f2e5
hanseltortiusdev <hansel.tertius@tortius.id>
2020-11-02 09:29:30 +0700

    Implement on optimizing assets

--- app/api/client.js
+++ app/api/client.js
@@ -3,7 +3,7 @@ import cache from '../utility/cache';
 import authStorage from '../auth/storage';
 
 const apiClient = create({
-    baseURL: 'http://192.168.100.54:9000/api'
+    baseURL: 'http://192.168.18.8:9000/api'
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
